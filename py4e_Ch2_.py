3c10c2dfd33dbee0287bd1235c8ce6719fea6d59
CQuinnce <canadaquinton1@gmail.com>
2023-08-30 22:37:02 -0500

    first commit

---
+++ py4e_Ch2_.py
@@ -0,0 +1,40 @@
+# --Exercises--
+
+"""Exercise 2: Write a program that uses input to prompt a user for their name and then welcomes them.""" # noqa E501
+
+# name = input("What is your name?\n")
+
+# print(f"Hello {name}")
+
+# ----
+
+"""Exercise 3: Write a program to prompt the user for hours and rate per hour to compute gross pay."""  # noqa E501
+"""
+    Enter Hours: 35
+    Enter Rate: 2.75
+    Pay: 96.25
+"""
+# hours = input("Enter hours: ")
+# rate = input("Enter rate: ")
+# hours = float(hours)
+# rate = float(rate)
+# pay = hours * rate
+# pay = round(pay)
+# print(f"Your expected pay is currently {pay}")
+
+# ----
+
+"""Exercise 4: Assume that we execute the following assignment statements:
+
+width = 17
+height = 12.0
+For each of the following expressions, write the value of the expression and the type (of the value of the expression)."""  # noqa E501
+
+# width//2
+
+# width/2.0
+
+# height/3
+
+# 1 + 2 * 5
+
